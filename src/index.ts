4f57556fe1dafdb9cd2d4089c03cb59fa66e9c89
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-30 14:48:11 -0300

    feat(index): add card routes and define port

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import express from "express";
+import cardRoute from "./routes/cards.route";
 import { AppDataSource } from "./data-source";
 
 AppDataSource.initialize().then(() => {
   const app = express();
-  const port = process.env.PORT;
+  const port = process.env.PORT || 5000;
 
   app.use(express.json());
 
-  app.get("/", (req, res) => {
-    return res.json("Hello, World!");
-  });
+  app.use("/cards", cardRoute);
 
   return app.listen(port, () =>
     console.log(`Servidor rodando na porta ${port}`)
