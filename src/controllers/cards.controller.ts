9797e5a07d341f503a36f7d3aea63a39b5c0412a
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-30 21:45:59 -0300

    refactor: add bad request error

--- src/controllers/cards.controller.ts
+++ src/controllers/cards.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { BadRequestError } from "../helpers/api-errors";
 import { cardRepository } from "../repositories/cards.repository";
 
 export class CardController {
@@ -6,19 +7,14 @@ export class CardController {
     const { titulo, conteudo, lista } = req.body;
 
     if (!titulo || !conteudo || !lista)
-      return res.status(400).send({
-        message: "Você não informou todos os campos para a criação de um card",
-      });
+      throw new BadRequestError(
+        "Você não informou todos os campos para a criação de um card"
+      );
 
-    try {
-      const newCard = cardRepository.create({ titulo, conteudo, lista });
+    const newCard = cardRepository.create({ titulo, conteudo, lista });
 
-      await cardRepository.save(newCard);
+    await cardRepository.save(newCard);
 
-      return res.status(201).send(newCard);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send({ message: "Internal Server Error" });
-    }
+    return res.status(201).send(newCard);
   }
 }
