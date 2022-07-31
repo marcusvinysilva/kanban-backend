a91b1216d7b994f36ea4ef4b6ba4c7d870151ecb
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-30 21:43:54 -0300

    chore: add error middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import "express-async-errors";
 import express from "express";
 import cardRoute from "./routes/cards.route";
 import { AppDataSource } from "./data-source";
+import { errorMiddleware } from "./middlewares/error.middleware";
 
 AppDataSource.initialize().then(() => {
   const app = express();
@@ -9,6 +11,8 @@ AppDataSource.initialize().then(() => {
   app.use(express.json());
 
   app.use("/cards", cardRoute);
+
+  app.use(errorMiddleware);
 
   return app.listen(port, () =>
     console.log(`Servidor rodando na porta ${port}`)
